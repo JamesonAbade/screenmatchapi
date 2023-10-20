c23e202d3cfdda0ea057be927664e0b2c3441a23
Jameson Abade dos Santos <128093017+JamesonAbade@users.noreply.github.com>
2023-10-20 10:19:20 -0300

    Add files via upload

---
+++ src/main/java/tk/vdcinformatica/screematchapi/ScreematchapiApplication.java
@@ -0,0 +1,13 @@
+package tk.vdcinformatica.screematchapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreematchapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreematchapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tk/vdcinformatica/screematchapi/ScreematchapiApplicationTests.java
@@ -0,0 +1,13 @@
+package tk.vdcinformatica.screematchapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreematchapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
